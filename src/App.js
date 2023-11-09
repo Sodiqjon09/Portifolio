47427a224a823e23ade886ee288dbf045279f0c1
Your Name <you@example.com>
2023-11-09 21:55:11 +0500

    navbar bitti

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Services";
+import Button from "./button_click/button";
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <Router> {/* BrowserRouter componentini o'zgartirish */}
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> {/* Services o'rniga Contact */}
+          <Route path="/button" element={<Button />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
